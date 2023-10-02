802de62e0e7349586711cf7c854efca459a40521
Luz <145450869+Q7nurs3vim@users.noreply.github.com>
2023-10-02 14:57:33 +0300

    Add files via upload

---
+++ ürün-satısV1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ürün_satısV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFiyat = new System.Windows.Forms.Label();
+            this.lblMiktar = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.lblTutar = new System.Windows.Forms.Label();
+            this.btnHesap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFiyat
+            // 
+            this.lblFiyat.AutoSize = true;
+            this.lblFiyat.Location = new System.Drawing.Point(27, 31);
+            this.lblFiyat.Name = "lblFiyat";
+            this.lblFiyat.Size = new System.Drawing.Size(53, 13);
+            this.lblFiyat.TabIndex = 0;
+            this.lblFiyat.Text = "Birim fiyatı";
+            // 
+            // lblMiktar
+            // 
+            this.lblMiktar.AutoSize = true;
+            this.lblMiktar.Location = new System.Drawing.Point(27, 76);
+            this.lblMiktar.Name = "lblMiktar";
+            this.lblMiktar.Size = new System.Drawing.Size(36, 13);
+            this.lblMiktar.TabIndex = 1;
+            this.lblMiktar.Text = "Miktar";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(113, 31);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(113, 73);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // lblTutar
+            // 
+            this.lblTutar.AutoSize = true;
+            this.lblTutar.Location = new System.Drawing.Point(30, 136);
+            this.lblTutar.Name = "lblTutar";
+            this.lblTutar.Size = new System.Drawing.Size(32, 13);
+            this.lblTutar.TabIndex = 4;
+            this.lblTutar.Text = "Tutar";
+            // 
+            // btnHesap
+            // 
+            this.btnHesap.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnHesap.Location = new System.Drawing.Point(97, 125);
+            this.btnHesap.Name = "btnHesap";
+            this.btnHesap.Size = new System.Drawing.Size(116, 35);
+            this.btnHesap.TabIndex = 5;
+            this.btnHesap.Text = "Fiyat hesapla";
+            this.btnHesap.UseVisualStyleBackColor = false;
+            this.btnHesap.Click += new System.EventHandler(this.btnHesap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(232, 204);
+            this.Controls.Add(this.btnHesap);
+            this.Controls.Add(this.lblTutar);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblMiktar);
+            this.Controls.Add(this.lblFiyat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFiyat;
+        private System.Windows.Forms.Label lblMiktar;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label lblTutar;
+        private System.Windows.Forms.Button btnHesap;
+    }
+}
+
---
+++ ürün-satısV1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ürün_satısV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesap_Click(object sender, EventArgs e)
+        {
+            int fiyat, miktar, tutar;
+            fiyat= Convert.ToInt32(txt1.Text);
+            miktar= Convert.ToInt32(txt2.Text);
+            tutar =fiyat* miktar;
+            lblTutar.Text= "TUTAR:" + tutar.ToString();
+
+        }
+    }
+}
